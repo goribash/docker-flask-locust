e3c7d23ed0de319c68cfcb712f2af1f1aca4e9c2
goribash <goribash@yahoo.com>
2022-08-14 04:59:39 +0000

    locust update

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,8 @@
-from locust import HttpLocust, TaskSet, between
+rom locust import HttpUser, between, task
 
-def index(l):
-    l.client.get("/")
-
-class UserBehavior(TaskSet):
-    tasks = {index: 1}
-
-class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
-    wait_time = between(5.0, 9.0)
+class WebsiteUser(HttpUser):
+    wait_time = between(5, 15)
+    
+    @task
+    def index(self):
+        self.client.get("/")
